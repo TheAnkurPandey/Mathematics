53f7f19efd52b087332db9a631396ebbfab5dd44
Ankur Pandey <ankur.pandey1996@gmail.com>
2018-03-19 13:49:08 +0530

    Numerical integration

--- SimpsonsThreeEight.cpp
+++ SimpsonsThreeEight.cpp
@@ -1,5 +1,5 @@
 //Numerical Integration
-//SIMPSONS ONE THIRD
+//SIMPSONS THREE EIGHT
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,46 +11,37 @@ float f(float x){
 }
 
 int main(){
-	float a,b,h,integr,mo,eo,no,t;
+	float a, b, h, integr, mo, eo, no, t;
 	int n;
-	
 	/*printf("Enter the limits/n");
-	scanf("%f%f", &a, &b);
+	scanf("%f%f",&a,&b);
 	printf("enter the n/n");
-	scanf("%f", &n);*/
+	scanf("%f",&n);*/
 	a = 0;
 	b = 1;
-	n = 10; //n has to be even
+	n = 6; //n has to be multiple of 3
 	
 	h = (b-a)/n;
-	printf("h = %f\n", h);
-	
-	printf("\n");
+	printf("Numerical Integration \n\nh = %f \n\n",h);
 	
 	float y[n+1];
-	for(int i = 0; i < n+1; i++) {
+	for(int i = 0; i < n+1; i++)	{
 		t = a + (i*h);
-		//printf("t = %f\n", t);
+		//printf("\nt = %f\n", t);
 		y[i] = f(t);
 	}
-	
-	printf("\n");
-	
 	for(int i = 0; i < n+1; i++)
 		printf("y[%d] = %f \n", i, y[i]);
 	
 	for(int i = 1; i < n; i++) {
-		if ( i%2 == 0 )
+		if( i%3 == 0 )
 			mo += y[i];
 		else
 			no += y[i];
 	}
 	eo = y[0] + y[n];
-	integr = (  h*( eo+ (2*mo) + (4*no) )  ) /3;
-	
-	printf("\n");
-	
-	printf("Integration of [f(x) =  1 / (1 + x^2)] is:  %f", integr);
+	integr = (  3*h*( eo+ (2*mo) + (3*no) )  ) /8;
+	printf("\nIntegration of [f(x) =  1 / (1 + x^2)] is:  %f", integr);
 	
 	return 0;
 }
